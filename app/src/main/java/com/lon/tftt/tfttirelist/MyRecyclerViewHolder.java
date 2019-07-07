6a3b0a8691a475048fb008e50813913d930bbe26
magelon <alfardproject@gmail.com>
2019-07-06 17:48:26 -0700

    c

--- app/src/main/java/com/lon/tftt/tfttirelist/MyRecyclerViewHolder.java
+++ app/src/main/java/com/lon/tftt/tfttirelist/MyRecyclerViewHolder.java
@@ -1,17 +1,31 @@
 package com.lon.tftt.tfttirelist;
 
 import android.view.View;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
 public class MyRecyclerViewHolder extends RecyclerView.ViewHolder {
     TextView txt_title,txt_comment;
+    ImageView imageView,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8;
     public MyRecyclerViewHolder(@NonNull View itemView) {
         super(itemView);
 
         txt_comment=(TextView)itemView.findViewById(R.id.txt_content);
         txt_title=(TextView)itemView.findViewById(R.id.txt_title);
+
+        imageView=(ImageView)itemView.findViewById(R.id.imageView);
+        imageView2=(ImageView)itemView.findViewById(R.id.imageView2);
+        imageView3=(ImageView)itemView.findViewById(R.id.imageView3);
+        imageView4=(ImageView)itemView.findViewById(R.id.imageView4);
+        imageView5=(ImageView)itemView.findViewById(R.id.imageView5);
+        imageView6=(ImageView)itemView.findViewById(R.id.imageView6);
+        imageView7=(ImageView)itemView.findViewById(R.id.imageView7);
+        imageView8=(ImageView)itemView.findViewById(R.id.imageView8);
+
     }
 }
--- app/src/main/java/com/lon/tftt/tfttirelist/TireList.java
+++ app/src/main/java/com/lon/tftt/tfttirelist/TireList.java
@@ -6,8 +6,12 @@ import androidx.annotation.NonNull;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -17,6 +21,7 @@ import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -28,38 +33,11 @@ public class TireList extends AppCompatActivity {
     FirebaseRecyclerOptions<Post> options;
     FirebaseRecyclerAdapter<Post, MyRecyclerViewHolder> adapter;
 
-
-
-    private TextView mTextMessage;
-
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
-                    return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
-                    return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
-                    return true;
-            }
-            return false;
-        }
-    };
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_tire_list);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+        setContentView(R.layout.activity_tire_list);
 
         recyclerView=(RecyclerView)findViewById(R.id.recycler_view);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
@@ -86,7 +64,10 @@ public class TireList extends AppCompatActivity {
        // databaseReference.push().setValue(post);
     }
 
+
+
     private void displayComment() {
+
         options =
                 new FirebaseRecyclerOptions.Builder<Post>()
                         .setQuery(databaseReference, Post.class)
@@ -97,12 +78,137 @@ public class TireList extends AppCompatActivity {
                     protected void onBindViewHolder(@NonNull MyRecyclerViewHolder myRecyclerViewHolder, int i, @NonNull Post post) {
                         myRecyclerViewHolder.txt_title.setText(post.getTitle());
                         myRecyclerViewHolder.txt_comment.setText(post.getContent());
+
+                        final ImageView imageView=myRecyclerViewHolder.imageView;
+                        final ImageView imageView2=myRecyclerViewHolder.imageView2;
+                        final ImageView imageView3=myRecyclerViewHolder.imageView3;
+                        final ImageView imageView4=myRecyclerViewHolder.imageView4;
+                        final ImageView imageView5=myRecyclerViewHolder.imageView5;
+                        final ImageView imageView6=myRecyclerViewHolder.imageView6;
+                        final ImageView imageView7=myRecyclerViewHolder.imageView7;
+                        final ImageView imageView8=myRecyclerViewHolder.imageView8;
                         //split the champions string into indevidul champions
                         String[] champions=post.getContent().split(",");
                         //convert champions to imgs
-                        for (int champion=0;champion<champions.length;champion++){
 
-                        }
+                        final DatabaseReference databaseReferenceImg=firebaseDatabase.getReference("champions");
+
+                        databaseReferenceImg.child(champions[0]).child("img").addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                    if(dataSnapshot.getValue()!=null){
+                                        String imgUrl=(String)dataSnapshot.getValue();
+
+                                        Picasso.get().load(imgUrl).into(imageView);
+                                    }
+
+                                }
+                                @Override
+                                public void onCancelled(@NonNull DatabaseError databaseError) {
+                                }
+
+                            });
+
+                        databaseReferenceImg.child(champions[6]).child("img").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.getValue()!=null){
+                                    String imgUrl=(String)dataSnapshot.getValue();
+
+                                    Picasso.get().load(imgUrl).into(imageView2);
+                                }
+
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                            }
+                        });
+
+                        databaseReferenceImg.child(champions[1]).child("img").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.getValue()!=null){
+                                    String imgUrl=(String)dataSnapshot.getValue();
+
+                                    Picasso.get().load(imgUrl).into(imageView3);
+                                }
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                            }
+                        });
+
+                        databaseReferenceImg.child(champions[2]).child("img").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.getValue()!=null){
+                                    String imgUrl=(String)dataSnapshot.getValue();
+
+                                    Picasso.get().load(imgUrl).into(imageView4);
+                                }
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                            }
+                        });
+
+                        databaseReferenceImg.child(champions[3]).child("img").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.getValue()!=null){
+                                    String imgUrl=(String)dataSnapshot.getValue();
+
+                                    Picasso.get().load(imgUrl).into(imageView5);
+                                }
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                            }
+                        });
+
+                        databaseReferenceImg.child(champions[4]).child("img").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.getValue()!=null){
+                                    String imgUrl=(String)dataSnapshot.getValue();
+
+                                    Picasso.get().load(imgUrl).into(imageView6);
+                                }
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                            }
+                        });
+
+                        databaseReferenceImg.child(champions[5]).child("img").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.getValue()!=null){
+                                    String imgUrl=(String)dataSnapshot.getValue();
+
+                                    Picasso.get().load(imgUrl).into(imageView7);
+                                }
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                            }
+                        });
+
+                        databaseReferenceImg.child(champions[7]).child("img").addListenerForSingleValueEvent(new ValueEventListener() {
+                            @Override
+                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                                if(dataSnapshot.getValue()!=null){
+                                    String imgUrl=(String)dataSnapshot.getValue();
+
+                                    Picasso.get().load(imgUrl).into(imageView8);
+                                }
+                            }
+                            @Override
+                            public void onCancelled(@NonNull DatabaseError databaseError) {
+                            }
+                        });
+
+                        //Picasso.get().load("https://firebasestorage.googleapis.com/v0/b/tfttierlist.appspot.com/o/tft%2Fashe.png?alt=media&token=0362ce86-7e52-4129-a268-dca171ecf94a").into(myRecyclerViewHolder.imageView);
 
                     }
 
