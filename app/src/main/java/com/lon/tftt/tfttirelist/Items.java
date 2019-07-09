89622df1bd0f39c2812976c92102daa26cf8d0b7
lon <alfardproject@gmail.com>
2019-07-09 14:01:46 -0700

    c

--- app/src/main/java/com/lon/tftt/tfttirelist/Items.java
+++ app/src/main/java/com/lon/tftt/tfttirelist/Items.java
@@ -2,14 +2,28 @@ package com.lon.tftt.tfttirelist;
 
 import android.os.Bundle;
 import android.os.PersistableBundle;
+import android.view.Menu;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 public class Items extends AppCompatActivity {
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
         setContentView(R.layout.activity_item_list);
+
+        Toolbar topToolBar=findViewById(R.id.toolbar);
+        setSupportActionBar(topToolBar);
     }
+
+    // Menu icons are inflated just as they were with actionbar
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
 }
